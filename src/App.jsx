3bf5eb0e3d9fddb73e3c997a73573faf05022875
Shaikh Mubashir Ahmed <shaikhmubashirahmed123@gmail.com>
2025-11-30 22:34:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Components/Header/Header'
+import Input from './Components/Header/Inputsfield/Input'
+
+function App() {
+
+  return (
+    <>
+    <center className='border-blue-400 md:w-[30%] w-[70%] mx-auto mt-10 p-5 rounded shadow-lg bg-white '>
+
+      <Header/>
+      <Input/>
+    </center>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='font-bold text-xl md:text-2xl text-blue-400 '>Expense Tracker</div>
+  )
+}
+
+export default Header
